fc982a494bbdc910f99acc17e9e94ef68c7d1909
Kenneth Payson <kenneth.payson@labshare.org>
2020-12-08 23:15:58 -0500

    inMemoryDB method

--- auth-query-issue/src/controllers/tenant.controller.ts
+++ auth-query-issue/src/controllers/tenant.controller.ts
@@ -5,15 +5,15 @@ import {
 
 } from '@loopback/rest';
 
-// import { inject } from '@loopback/core';
+import {omit} from 'lodash';
+
 import { Tenant } from '../models';
 import {
-  TenantRepository
+  TenantRepository,
+  ClientRepository,
+  ClientIdentityProviderRepository,
+  IdentityProviderRepository
 } from '../repositories';
-import {
-  TenantAllDataService,
-
-} from '../services';
 
 
 export class TenantController {
@@ -21,10 +21,13 @@ export class TenantController {
 
     @repository(TenantRepository)
     public tenantRepository: TenantRepository,
-    // @inject(TenantBindings.TENANT_CREATION_SERVICE)
-    // private tenantCreationService: TenantCreationService,
-    // @inject(TenantBindings.TENANT_ALL_DATA_SERVICE)
-    // private tenantAllDataService: TenantAllDataService,
+    @repository(ClientRepository)
+    public clientRepository: ClientRepository,
+    @repository(IdentityProviderRepository)
+    public identityProviderRepository: IdentityProviderRepository,
+    @repository(ClientIdentityProviderRepository)
+    public clientIdentityProviderRepository: ClientIdentityProviderRepository,
+
 
   ) {}
 
@@ -41,9 +44,7 @@ export class TenantController {
     }
   })
   public async find(): Promise<Tenant[]> {
-
     return this.tenantRepository.find();
-    //return this.userRepository.tenants(this.request.user.sub).find(filter);
   }
 
   @get('/auth/admin/tenants/{tenantId}/export', {
@@ -79,31 +80,73 @@ export class TenantController {
     return this.tenantRepository.findById(tenantId, filter);
   }
 
-  // @post('/auth/admin/tenants/import', {
-  //   responses: {
-  //     200: {
-  //       description: 'Tenant model with all dependencies'
-  //     }
-  //   }
-  // })
-  // public async importTenantData(
-  //   @requestBody({
-  //     required: true
-  //   })
-  //   allData: any
-  // ): Promise<any> {
-  //   const adminUserIds = [];
-  //   for (const admin of settingsConfig['auth'].system.administrators || []) {
-  //     const user = await this.userRepository.findOne({
-  //       where: { email: admin.email }
-  //     });
-  //     adminUserIds.push(user.id);
-  //   }
+  @get('/auth/admin/tenants/{tenantId}/inMemoryDB', {
+    responses: {
+      200: {
+        description: 'export json for in memory db',
+        content: { 'application/json': { schema: { 'x-ts-type': Tenant } } }
+      }
+    }
+  })
+  public async inMemoryDB(
+    @param.path.number('tenantId') tenantId: number
+  ): Promise<any> {
 
-  //   const currentUserId = Number(this.request.user.sub);
-  //   if (!adminUserIds.some((uid) => uid === currentUserId)) {
-  //     adminUserIds.push(currentUserId);
-  //   }
-  //   return this.tenantCreationService.createAll(allData, adminUserIds);
-  // }
+    const toInMemoryFormat = (dataArray:any[]) => {
+      const escape =  (v:any) => JSON.stringify(v).replace(/\"/g,"\\\"");
+
+      const inMemoryData =
+        dataArray.reduce((db,v,i)=>({...db,[i]:escape(v)}),{});
+      return inMemoryData;
+    }
+// scope:{fields:{id:true,title:true,tenantId:true}}
+    // const tenantFilter = {where:{id:tenantId}} as Filter<Tenant>;
+    const tenants = await this.tenantRepository.find();
+    const inMemoryTenants = toInMemoryFormat(tenants);
+
+    const clientFilter = {where:{tenantId},
+      scope:{
+        fields:{id:true,clientId:true,name:true},
+        include:[ {
+          relation: 'providers',
+          scope: {fields:{id:true, clientId:true, identityProviderId:true}}
+        }
+    ]}};
+    const clientsWithProviders = await this.clientRepository.find(clientFilter);
+    const clients = clientsWithProviders.map(x=>omit(x,['providers']));
+    const clientProviders = clientsWithProviders.flatMap(x=> x.providers);
+
+    const inMemoryClients = toInMemoryFormat(clients);
+    const inMemoryClientProviders = toInMemoryFormat(clientProviders);
+
+    const identityProvidersFilter = {where:{tenantId},scope:{fields:{id:true,name:true,tenantId:true}}};
+    const identityProviders = await this.identityProviderRepository.find(identityProvidersFilter);
+    const inMemoryIdentityProviders = toInMemoryFormat(identityProviders);
+
+    const db = {
+      ids: {
+        Tenant: tenants.length + 1,
+        Client: clients.length + 1,
+        IdentityProvider: identityProviders.length+1,
+        ClientIdentityProvider: clientProviders.length+1
+      },
+      models: {
+        Tenant: inMemoryTenants,
+        Client: inMemoryClients,
+        IdentityProvider: inMemoryIdentityProviders,
+        ClientIdentityProvider: inMemoryClientProviders
+      }
+    }
+
+    const fs = require('fs');
+    fs.writeFile('~/inMemDB.json', JSON.stringify(db), (err: any) => {
+        console.log(err);
+    });
+
+    return "success";
+  }
+
+
+
+
 }
